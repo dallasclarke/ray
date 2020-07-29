72efddbc37b6bb1fada628141e15ad220474fc51
Dallas Clarke <dallas.clarke@codeimmersives.com>
2020-07-29 12:01:44 -0700

    lab

--- main.js
+++ main.js
@@ -1,5 +1,44 @@
 const Ray = function() {
-  
+  return {
+      length: 0,
+
+      push: function(value) {
+        this[this.length] = value;
+        this.length++;
+    },
+        pop: function() {
+            const lastIndex = this.length -1;
+            const toBeRemoved = this[lastIndex];
+            delete this[lastIndex]
+            this.length--;
+            
+            return toBeRemoved;
+        },
+
+        includes: function(value) {
+            for(let i = 0; i < this.length; i++) {
+                if (this[i] === value) {
+                    return true;
+                }
+            }
+            return false;
+        },
+
+        indexOf: function(value) {
+            for (let i = 0; i < this.length; i++) {
+                if (this[i] === value)
+                    return i;
+            }
+            return -1;
+        },
+
+        shift: function() {
+            const firstIndex = this[0];
+            
+        }
+        
+  }
+  return ray
 }
 
 
